d56d52fe034ad615482a5e2a4b8a86b4d3cf7e81
Fabrizio Cirelli <cirelli94@gmail.com>
2020-03-13 11:31:34 +0100

    Refactor onion_writer to use helper

--- onionwriter/onion_writer.go
+++ onionwriter/onion_writer.go
@@ -5,22 +5,17 @@ import (
 	"io"
 
 	"github.com/goraz/onion"
-	"github.com/imdario/mergo"
+	"github.com/goraz/onion/helper"
 )
 
 // SerializeOnion try to serialize the onion into a json stream.
 // TODO : Add more option, maybe support for more format. (Do we need more option on writing?)
 func SerializeOnion(o *onion.Onion, w io.Writer) error {
 	data := o.LayersData()
-	res := make(map[string]interface{})
-	for i := range data {
-		err := mergo.Merge(&res, data[i], mergo.WithOverride)
-		if err != nil {
-			return err
-		}
-	}
+
+	mergedData := helper.MergeLayersData(data)
 
 	enc := json.NewEncoder(w)
 	enc.SetIndent("", "  ")
-	return enc.Encode(res)
+	return enc.Encode(mergedData)
 }
